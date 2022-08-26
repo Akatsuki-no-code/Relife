4996b07fbb6be57314a606e64e3980922d6c560a
Suraj S <qwerky2003@gmail.com>
2022-08-27 00:56:21 +0530

    Updated with images

--- src/pages/aboutus/aboutus.js
+++ src/pages/aboutus/aboutus.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import "./styles/aboutus.css"
-import img from './img/rahul.svg'
+import rahul from './img/rahul.svg'
+import dhruv from './img/dhruv.svg'
+import suraj from './img/suraj.svg'
+import lasya from './img/lasya.svg'
+import ajai from './img/ajai.svg'
 
 const AboutUs = () => {
     return (
@@ -9,75 +13,50 @@ const AboutUs = () => {
                 <p> "If I have seen further, it is by standing on the shoulders of giants." <br />-Isaac Newton</p>
             </div>
             <>
-                <h2 style={{ textAlign: "center" }}>Our Team</h2>
+                <h2 style={{ textAlign: "center" }}>Meet Our Team</h2>
+                <div className="row">
+                    <div className="column-1">
+                        <div className="card">
+                            <img src={rahul} alt="Mike" style={{ width: "100%" }} />
+                            <div className="container">
+                                <h2>Rahul Diwakar</h2>
+                                <div className='details' >
+                                    <p className="title">Team Leader</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div className="row">
                     <div className="column">
                         <div className="card">
-                        <img src= {img}alt="Mike" style={{ width: "100%" }} />
-                            <div className="container">
-                                <h2>Rahul Diwakar</h2>
-                                <p className="title">Team Leader</p>
-                                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                                <p>jane@example.com</p>
-                                <p>
-                                    <button className="button">Contact</button>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="column">
-                        <div className="card">
-                            <img src="/w3images/team2.jpg" alt="Mike" style={{ width: "100%" }} />
+                            <img src={dhruv} alt="Mike" style={{ width: "100%" }} />
                             <div className="container">
                                 <h2>Dhruv Millu</h2>
-                                <p className="title">Programmer</p>
-                                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                                <p>mike@example.com</p>
-                                <p>
-                                    <button className="button">Contact</button>
-                                </p>
                             </div>
                         </div>
                     </div>
                     <div className="column">
                         <div className="card">
-                            <img src="/w3images/team3.jpg" alt="John" style={{ width: "100%" }} />
+                            <img src={suraj} alt="John" style={{ width: "100%" }} />
                             <div className="container">
                                 <h2>Suraj S</h2>
-                                <p className="title">Designer</p>
-                                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                                <p>john@example.com</p>
-                                <p>
-                                    <button className="button">Contact</button>
-                                </p>
                             </div>
                         </div>
                     </div>
                     <div className="column">
                         <div className="card">
-                            <img src="/w3images/team3.jpg" alt="John" style={{ width: "100%" }} />
+                            <img src={lasya} alt="John" style={{ width: "100%" }} />
                             <div className="container">
                                 <h2>Sai Lasya</h2>
-                                <p className="title">Designer</p>
-                                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                                <p>john@example.com</p>
-                                <p>
-                                    <button className="button">Contact</button>
-                                </p>
                             </div>
                         </div>
                     </div>
                     <div className="column">
                         <div className="card">
-                            <img src="/w3images/team3.jpg" alt="John" style={{ width: "100%" }} />
+                            <img src={ajai} alt="John" style={{ width: "100%" }} />
                             <div className="container">
                                 <h2>Ajai Raj</h2>
-                                <p className="title">Designer</p>
-                                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                                <p>john@example.com</p>
-                                <p>
-                                    <button className="button">Contact</button>
-                                </p>
                             </div>
                         </div>
                     </div>
